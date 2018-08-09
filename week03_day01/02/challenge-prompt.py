99d84e4434e4c6008470ced581ec67999f21bc20
Elize Papineau <elizepapineau@gmail.com>
2018-08-08 22:57:10 -0700

    drill 02

---
+++ week03_day01/02/challenge-prompt.py
@@ -0,0 +1,140 @@
+#Variables
+# The nouns of programming
+
+# This is a comment - any code commented out is not run -
+# meaning that any code or text commented out is ignored.
+
+"""
+this is a multiline comment
+You can coment out many lines at once.
+For the most part you will see single line comments
+but it is good to see this.
+As in single line comments, any code contained within multi-line comments are not run -
+meaning that any code commented out is ignored.
+"""
+
+#Topic: strings
+
+# Create a variable named subject with no value:
+# Example: hobby = None
+subject = None
+
+# Assign a value of "Programmers" to the variable subject
+# Example: hobby = "problem solving"
+subject = "Programmers"
+
+# Create a variable, first_name, and assign it a value of an empty string.
+# Example: empty_string = ""
+first_name = ""
+
+# Assign a value of "Ada" to the variable first_name
+first_name = "Ada"
+
+
+# Create a variable, last_name, and assign it a value of a string, "Lovelace".
+last_name = "Lovelace"
+
+# Create a variable, full_name, and assign it a value of the combination of first_name and last_name with a space.
+full_name = first_name + " " + last_name
+
+
+# Create a variable, profession, and assign it a value of a string, "Computer Programmer".
+profession = "Computer Programmer"
+
+
+# Create a variable, known_for, and assign it a value of a string, "First Computer Programmer".
+known_for = "First Computer Programmer"
+
+
+# Create a variable, first_algorithm, and assign it a value of a string, "Analytical Engine".
+first_algorithm = "Analytical Engine"
+
+
+# Create a variable, city_location, and assign it a value of a string, "London";
+city_location = "London"
+
+
+# Create a variable, country_location, and assign it a value of a string, "England";
+country_location = "England"
+
+# Create a variable, nationality, and assign it a value of a string, "British";
+nationality = "British"
+
+
+#Topic: integers
+
+# Create a variable, birth_year, and assign it with an integer of 1815.
+birth_year = 1815
+
+
+# Create a variable, death_year, and assign it with an integer of 1852.
+death_year = 1852
+
+
+# Create a variable, age_at_passing, and assign it a value of death_year minus birth_year.
+age_at_passing = death_year - birth_year
+
+
+# Create a variable, year_of_publish, and assign it with an integer of 1842.
+year_of_publish = 1842
+
+
+#Topic: print
+
+##print: "First Name:" and first_name
+print(f"First Name: {first_name}")
+
+
+##print: "Last Name:" and last_name
+print(f"Last Name: {last_name}")
+
+
+##print: "Profession: " and profession
+print(f"Profession: {profession}")
+
+
+##print: "BirthYear: " and birth_year
+print(f"Birth Year: {birth_year}")
+
+
+#Topic: Concat Values
+
+# Create a variable, statement_one, and assign it a value of a string:
+# "Programmers: Ada Lovelace is a British Computer Programmer born in 1815."
+statement_one = "Programmers: Ada Lovelace is a British Computer Programmer born in 1815."
+
+
+# Create a variable, statement_two, and assign it a value of a string:
+# "She is commonly referred to as the first computer programmer"
+statement_two = "She is commonly referred to as the first computer programmer"
+
+
+
+# Create a variable, statement_three, and assign it a value of a string:
+# "In 1842 she published the first Algorithm, the Analytical Engine, at the age of 27."
+statement_three = "In 1842 she published the first Algorithm, the Analytical Engine, at the age of 27."
+
+
+# Create a variable, statement_four, and assign it a value of a string:
+# "She was a British Citizen who lived in London, England until her passing in 1852 at the age of 37."
+statement_four = "She was a British Citizen who lived in London, England until her passing in 1852 at the age of 37."
+
+
+#Topic: print()
+
+
+#print statement_one
+print(statement_one)
+
+
+#print statement_two
+print(statement_two)
+
+
+#print statement_three
+print(statement_three)
+
+
+#print statement_four
+print(statement_four)
+

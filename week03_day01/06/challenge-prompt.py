7d4ed84b45a4c546425c12a038d7e3121baf2cca
Elize Papineau <elizepapineau@gmail.com>
2018-08-09 14:29:14 -0700

    drill 06

---
+++ week03_day01/06/challenge-prompt.py
@@ -0,0 +1,45 @@
+# create a list, list_1,  with 0, 1 ,2 ,3 as values
+list_1 = [0, 1, 2, 3]
+
+# create list, list_2 with 4,5,6,7 as values
+list_2 = [4, 5, 6, 7]
+# create list, list_3 with 8,9,10,11 as values
+list_3 = [8, 9, 10, 11]
+# create list, list_4 with 12,13,14,15 as values
+list_4 = [12, 13, 14, 15]
+
+
+
+# print the first list_1
+print(list_1)
+
+# print each index of the list_1
+for item in list_1:
+    print(list_1.index(item))
+
+
+
+
+# print the second list_2
+print(list_2)
+# print each index of the second list_2
+for item in list_2:
+    print(list_2.index(item))
+
+
+
+
+# print the third list_3
+print(list_3)
+# print each index of the third list_3
+for item in list_3:
+    print(list_3.index(item))
+
+
+
+
+# print the fourth list_4
+print(list_4)
+# print each index of the fourth list_4
+for item in list_4:
+    print(list_4.index(item))

0bf89e299f69cb42fc8462d74deade0f5d4727ed
Elize Papineau <elizepapineau@gmail.com>
2018-08-10 00:41:39 -0700

    drill 03

---
+++ week03_day03/03/challenge.py
@@ -0,0 +1,50 @@
+# Recreate the following object without actually writing it explicitly. To do this, you will need to use bracket notation. You may not uncomment the object or use curly braces. Instead, you must only add to the empty object given to you using brackets.
+
+# san_francisco = {
+#     "west_coast": True,
+#     "has_multiple_bridges": True,
+#     "known_for_pizza": False,
+#     "coastal": True,
+#     "snows": False,
+#     "very_hot": False,
+#     "mayor": "London Breed",
+#     "state": "California",
+#     "country": "USA",
+#     "best_food": "burritos",
+#     "sports_teams": ["Giants", "Warriors", "Forty-Niners"],
+#     "tallest_building": "SalesForce Building",
+#     "population": 884363,
+#     "city_size": "large",
+#     "median_house_price": 1610000,
+#     "famous_residents": ["Maya Angelou", "Robert Frost", "Carlos Santana"],
+#     "homeless_pop": 1150,
+#     "politica_leaning": "Democrat",
+#     "notable_attractions": ["Alcatraz", "Golden Gate Bridge", "Fisherman's Wharf"],
+#     "Natural Disasters": ["Earthquakes"],
+# }
+
+san_francisco = {}
+# we'll give you the first one ;)
+san_francisco["west_coast"] = True
+san_francisco["has_multiple_bridges"] = True
+san_francisco["known_for_pizza"] = False
+san_francisco["coastal"] = True
+san_francisco["snows"] = False
+san_francisco["very_hot"] = False
+san_francisco["mayor"] = "London Breed"
+san_francisco["state"] = "California"
+san_francisco["country"] = "USA"
+san_francisco["best_food"] = "burritos"
+san_francisco["sports_team"] = ["Giants", "Warriors", "Forty-Niners"]
+san_francisco["tallest_building"] = "SalesForce Building"
+san_francisco["population"] = 884363
+san_francisco["city_size"] = "large"
+san_francisco["median_house_price"] = 1610000
+san_francisco["famous residents"] = ["Maya Angelou", "Robert Frost", "Carlos Santana"]
+san_francisco["homeless_pop"] = 1150
+san_francisco["political_leaning"] = "Democrat"
+san_francisco["notable_attractions"] = ["Alcatraz", "Golden Gate Bridge", "Fisherman's Wharf"]
+san_francisco["Natural_disasters"] = ["Earthquakes"]
+
+
+print(san_francisco)
